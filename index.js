4bd1b3c208058eb823a5862bc51306107ecacf22
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-12 09:33:51 -0700

    fixed dot

--- index.js
+++ index.js
@@ -37,7 +37,6 @@ function initLocalClocks() {
 function setUpMinuteHands() {
   var secondAngle = containers[0].getAttribute('data-second-angle');
   if(secondAngle > 0) {
-    // Set a timeout until the end of the current minute, to move the hand
     var delay = (((360 - secondAngle) / 6) + 0.1) * 1000;
     setTimeout(function() {
       moveMinuteHands(containers);
